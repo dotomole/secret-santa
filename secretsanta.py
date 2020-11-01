623ef093730bfa3f27d8d96598c2c65fab6ffd25
Tom Di Pietro <40588156+dotomole@users.noreply.github.com>
2020-11-01 17:24:29 +0800

    Nicer email template

--- secretsanta.py
+++ secretsanta.py
@@ -24,31 +24,98 @@ def getPeople(filename):
         for a_contact in contacts_file:
             people.append(a_contact)
     return people
-
 def sendMessage(receiver_first, receiver_last, receiver_email, toBuyFirst, toBuyLast):
     port = 587  # For starttls
     smtp_server = "smtp.gmail.com"
 
     message = MIMEMultipart()
-    message["Subject"] = "Secret Santa for {fname} {lname}".format(fname=receiver_first, lname=receiver_last)
+    message["Subject"] = "Secret Santa for {fname} {lname} - 2020".format(fname=receiver_first, lname=receiver_last)
     message["From"] = "Secret Santa"
     message["To"] = receiver_email
 
     html = """\
-    <html>
-      <body>
-        <p>Dear {fname} {lname},<br><br>
-        You have been invited to this year's <b>Secret Santa!</b><br><br>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ 
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  
+</head>
 
-        You have been given <u><b>{fname2} {lname2}</b></u> to buy a gift for.<br><br> 
+<body yahoo bgcolor="#ffebeb" style="margin: 0; padding: 0; min-width: 100%!important;">
+<table width="100%" bgcolor="#ffebeb" border="0" cellpadding="0" cellspacing="0">
+<tr>
+  <td>
+    <!--[if (gte mso 9)|(IE)]>
+      <table width="600" align="center" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+          <td>
+    <![endif]-->     
+    <table bgcolor="#ffffff" class="content" align="center" cellpadding="0" cellspacing="0" border="0" style="width: 100%; max-width: 600px;">
+      <tr>
+        <td bgcolor="#d96666" class="header" style="padding: 40px 30px 20px 30px;">
+          <table width="70" align="left" border="0" cellpadding="0" cellspacing="0">  
+            <tr>
+              <td height="70" style="padding: 0 20px 20px 0;">
+                <img style="height: auto;" class="fix" src="https://i.pinimg.com/originals/d7/5a/03/d75a03be8c8462a7f0abdea69c685e36.png" width="70" height="70" border="0" alt="" />
+              </td>
+            </tr>
+          </table>
+          <!--[if (gte mso 9)|(IE)]>
+            <table width="425" align="left" cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                <td>
+          <![endif]-->
+          <table class="col425" align="left" border="0" cellpadding="0" cellspacing="0" style="width: 100%; max-width: 425px;">  
+            <tr>
+              <td height="70">
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                  <tr>
+                    <td class="subhead" style="padding: 0 0 0 3px;font-size: 15px; color: #ffffff; font-family: sans-serif; letter-spacing: 10px;">
+                      WELCOME TO
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="h1" style="padding: 5px 0 0 0;color: #153643; font-family: sans-serif;font-size: 33px; line-height: 38px; font-weight: bold;">
+                      Secret Santa - 2020
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+          <!--[if (gte mso 9)|(IE)]>
+                </td>
+              </tr>
+          </table>
+          <![endif]-->
+        </td>
+      </tr>
+      <tr>
+        <td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px;border-bottom: 1px solid #f2eeed;">
+          <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
 
-        Merry Christmas!<br><br>
-
-        Love, <br>
-        Secret Santa.
-        </p>
-      </body>
-    </html>
+            </tr>
+            <tr>
+              <td class="bodycopy" style="color: #153643; font-family: sans-serif;font-size: 16px; line-height: 22px;">
+                Dear <b>{fname} {lname}</b>,
+                <br>
+                You have been invited to this year's Secret Santa. There will be a <b>$50</b> limit.
+                <br><br>
+                <text style="text-align:center">You must buy a present for:</text>
+              </td>
+            </tr>
+            <td class="h2" style="text-align:center;color: #153643; font-family: sans-serif;padding: 0 0 15px 0; font-size: 24px; line-height: 28px; font-weight: bold;">
+              <br>
+              {fname2} {lname2}
+            </td>
+          </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
     """.format(fname=receiver_first, lname=receiver_last, fname2=toBuyFirst.upper(), lname2=toBuyLast.upper())
 
     textMsg = MIMEText(html, "html")
@@ -111,13 +178,14 @@ def main():
             j+=1
         i+=1
 
+    k=0
     #Send all the emails
     for pick in picks:
-        print("sending email "+str(i+1)+" ...")
+        print("Sending Email "+str(k+1)+" ...")
         sendMessage(pick.firstName, pick.lastName, pick.email, pick.toBuyFirst, pick.toBuyLast)
-        i+=1
+        k+=1
     
 if __name__ == '__main__':
-    sender_email = input("Enter your email address: ")
-    password = input("Enter your email password: ")
+    sender_email = input("Enter your Email Address: ")
+    password = input("Enter your Email Password: ")
     main()
