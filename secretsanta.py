561465d1e729e9f5eb28c8743316a56dccfb1f00
Tom Di Pietro <40588156+dotomole@users.noreply.github.com>
2020-11-03 20:20:42 +0800

    Update secretsanta.py

--- secretsanta.py
+++ secretsanta.py
@@ -34,88 +34,74 @@ def sendMessage(receiver_first, receiver_last, receiver_email, toBuyFirst, toBuy
     message["To"] = receiver_email
 
     html = """\
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
- 
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  
-</head>
-
-<body yahoo bgcolor="#ffebeb" style="margin: 0; padding: 0; min-width: 100%!important;">
-<table width="100%" bgcolor="#ffebeb" border="0" cellpadding="0" cellspacing="0">
-<tr>
-  <td>
-    <!--[if (gte mso 9)|(IE)]>
-      <table width="600" align="center" cellpadding="0" cellspacing="0" border="0">
-        <tr>
-          <td>
-    <![endif]-->     
-    <table bgcolor="#ffffff" class="content" align="center" cellpadding="0" cellspacing="0" border="0" style="width: 100%; max-width: 600px;">
-      <tr>
-        <td bgcolor="#d96666" class="header" style="padding: 40px 30px 20px 30px;">
-          <table width="70" align="left" border="0" cellpadding="0" cellspacing="0">  
-            <tr>
-              <td height="70" style="padding: 0 20px 20px 0;">
-                <img style="height: auto;" class="fix" src="https://i.pinimg.com/originals/d7/5a/03/d75a03be8c8462a7f0abdea69c685e36.png" width="70" height="70" border="0" alt="" />
-              </td>
-            </tr>
-          </table>
-          <!--[if (gte mso 9)|(IE)]>
-            <table width="425" align="left" cellpadding="0" cellspacing="0" border="0">
-              <tr>
-                <td>
-          <![endif]-->
-          <table class="col425" align="left" border="0" cellpadding="0" cellspacing="0" style="width: 100%; max-width: 425px;">  
-            <tr>
-              <td height="70">
-                <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                  <tr>
-                    <td class="subhead" style="padding: 0 0 0 3px;font-size: 15px; color: #ffffff; font-family: sans-serif; letter-spacing: 10px;">
-                      WELCOME TO
+    <html>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body yahoo bgcolor="#ffebeb" style="margin: 0; padding: 0; min-width: 100%!important;">
+    <table width="100%" bgcolor="#ffebeb" border="0" cellpadding="0" cellspacing="0">
+    <tr>
+      <td>
+        <table bgcolor="#ffffff" class="content" align="center" cellpadding="0" cellspacing="0" border="0" style="width: 100%; max-width: 600px;">
+          <tr>
+            <td bgcolor="#d96666" class="header" style="padding: 40px 30px 20px 30px;">
+              <table width="70" align="left" border="0" cellpadding="0" cellspacing="0">  
+                <tr>
+                  <td height="70" style="padding: 0 20px 20px 0;">
+                    <img style="height: auto;" class="fix" src="https://i.pinimg.com/originals/d7/5a/03/d75a03be8c8462a7f0abdea69c685e36.png" width="70" height="70" border="0" alt="" />
+                  </td>
+                </tr>
+              </table>
+              <table class="col425" align="left" border="0" cellpadding="0" cellspacing="0" style="width: 100%; max-width: 425px;">  
+                <tr>
+                  <td height="70">
+                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                      <tr>
+                        <td class="subhead" style="padding: 0 0 0 3px;font-size: 15px; color: #ffffff; font-family: sans-serif; letter-spacing: 10px;">
+                          WELCOME TO
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="h1" style="padding: 5px 0 0 0;color: #153643; font-family: sans-serif;font-size: 33px; line-height: 38px; font-weight: bold;">
+                          Secret Santa - 2020
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+              <!--[if (gte mso 9)|(IE)]>
                     </td>
                   </tr>
-                  <tr>
-                    <td class="h1" style="padding: 5px 0 0 0;color: #153643; font-family: sans-serif;font-size: 33px; line-height: 38px; font-weight: bold;">
-                      Secret Santa - 2020
-                    </td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </table>
-          <!--[if (gte mso 9)|(IE)]>
-                </td>
-              </tr>
-          </table>
-          <![endif]-->
-        </td>
-      </tr>
-      <tr>
-        <td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px;border-bottom: 1px solid #f2eeed;">
-          <table width="100%" border="0" cellspacing="0" cellpadding="0">
-            <tr>
-
-            </tr>
-            <tr>
-              <td class="bodycopy" style="color: #153643; font-family: sans-serif;font-size: 16px; line-height: 22px;">
-                Dear <b>{fname} {lname}</b>,
-                <br>
-                You have been invited to this year's Secret Santa. There will be a <b>$50</b> limit.
-                <br><br>
-                <text style="text-align:center">You must buy a present for:</text>
-              </td>
-            </tr>
-            <td class="h2" style="text-align:center;color: #153643; font-family: sans-serif;padding: 0 0 15px 0; font-size: 24px; line-height: 28px; font-weight: bold;">
-              <br>
-              {fname2} {lname2}
+              </table>
+              <![endif]-->
             </td>
-          </table>
-        </td>
-    </tr>
-</table>
-</body>
-</html>
+          </tr>
+          <tr>
+            <td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px;border-bottom: 1px solid #f2eeed;">
+              <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+
+                </tr>
+                <tr>
+                  <td class="bodycopy" style="color: #153643; font-family: sans-serif;font-size: 16px; line-height: 22px;">
+                    Dear <b>{fname} {lname}</b>,
+                    <br>
+                    You have been invited to this year's Secret Santa. There will be a <b>$50</b> limit.
+                    <br><br>
+                    <text style="text-align:center">You must buy a present for:</text>
+                  </td>
+                </tr>
+                <td class="h2" style="text-align:center;color: #153643; font-family: sans-serif;padding: 0 0 15px 0; font-size: 24px; line-height: 28px; font-weight: bold;">
+                  <br>
+                  {fname2} {lname2}
+                </td>
+              </table>
+            </td>
+        </tr>
+    </table>
+    </body>
+    </html>
     """.format(fname=receiver_first, lname=receiver_last, fname2=toBuyFirst.upper(), lname2=toBuyLast.upper())
 
     textMsg = MIMEText(html, "html")
